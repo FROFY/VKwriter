de5c5bf9eff7c5948f8205bfb4d5dbc69c9a1c1a
FROFY <60229897+FROFY@users.noreply.github.com>
2020-07-31 23:29:22 +0300

    Add files via upload

---
+++ VkWritting/VkWritting/Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VkWritting
+{
+    public class Peer
+    {
+        public int id { get; set; }
+        public string type { get; set; }
+        public int local_id { get; set; }
+    }
+    public class SortId
+    {
+        public int major_id { get; set; }
+        public int minor_id { get; set; }
+    }
+    public class CanWrite
+    {
+        public bool allowed { get; set; }
+    }
+    public class Item
+    {
+        public Peer peer { get; set; }
+        public int last_message_id { get; set; }
+        public int in_read { get; set; }
+        public int out_read { get; set; }
+        public SortId sort_id { get; set; }
+        public bool is_marked_unread { get; set; }
+        public bool important { get; set; }
+        public CanWrite can_write { get; set; }
+    }
+    public class OnlineInfo
+    {
+        public bool visible { get; set; }
+        public int last_seen { get; set; }
+        public bool is_online { get; set; }
+        public int app_id { get; set; }
+        public bool is_mobile { get; set; }
+    }
+    public class Profile
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public bool is_closed { get; set; }
+        public bool can_access_closed { get; set; }
+        public int sex { get; set; }
+        public string screen_name { get; set; }
+        public string photo_50 { get; set; }
+        public string photo_100 { get; set; }
+        public int online { get; set; }
+        public OnlineInfo online_info { get; set; }
+    }
+    public class Response
+    {
+        public int count { get; set; }
+        public List<Item> items { get; set; }
+        public List<Profile> profiles { get; set; }
+    }
+
+    public class Root
+    {
+        public Response response { get; set; }
+    }
+}
---
+++ VkWritting/VkWritting/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using xNet;
+
+namespace VkWritting
+{
+    public partial class Form1 : Form
+    {
+        public bool check = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            while (check)
+            {
+                var request = new HttpRequest();
+                string response = request.Get("https://api.vk.com/method/messages.searchConversations?q=" + " " + "&count=" + textBox2.Text + "&extended=1" + "&access_token=" + textBox1.Text + "&v=5.122").ToString();
+                request.Close();
+                richTextBox1.Clear();
+                Root root;
+                root = JsonConvert.DeserializeObject<Root>(response);
+                for (int i = 0; i < root.response.count; i++)
+                {
+                    richTextBox1.Text += root.response.items[i].peer.id.ToString() + '\n';
+                    if (root.response.items[i].peer.id < 0)
+                        root.response.items[i].peer.id = Math.Abs(root.response.items[i].peer.id);
+                    Send(root.response.items[i].peer.id, root.response.profiles[i].first_name, root.response.profiles[i].last_name);
+                }
+            };
+        }
+
+        public void Send(int id, string fname, string lname)
+        {
+            var request = new HttpRequest();
+            string response = request.Get("https://api.vk.com/method/messages.setActivity?user_id=" + id + "&type=audiomessage" + "&access_token=" + textBox1.Text + "&v=5.122").ToString();
+            request.Close();
+            richTextBox2.Text += "Пользователю " + fname + " " + lname + " записывается голосовое сообщение..." + '\n' + response;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            check = false;
+        }
+    }
+}
